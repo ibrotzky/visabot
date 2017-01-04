dc9f928d8e4e8d5d2ac8cef640764b8f20b6d68b
Bruno Miranda <bvpmiranda@mac.com>
2017-01-04 21:59:49 -0200

    test

---
+++ vendor/heroku/heroku-buildpack-php/support/build/_util/include/manifest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+print_or_export_manifest_cmd() {
+    if [[ "${MANIFEST_CMD:-}" ]]; then
+        echo "$1" > $MANIFEST_CMD
+    else
+        echo "-----> Done. Run '$1' to upload manifest."
+    fi
+}
+
+generate_manifest_cmd() {
+    echo "s3cmd --ssl${AWS_ACCESS_KEY_ID+" --access_key=\$AWS_ACCESS_KEY_ID"}${AWS_SECRET_ACCESS_KEY+" --secret_key=\$AWS_SECRET_ACCESS_KEY"} --acl-public -m application/json put $(pwd)/${1} s3://${S3_BUCKET}/${S3_PREFIX}${1}"
+}
