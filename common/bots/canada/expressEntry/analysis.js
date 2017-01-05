15210ea2f78b9c33bc487b48bd162f2570fbdb66
Bruno Miranda <bvpmiranda@mac.com>
2017-01-05 12:12:33 -0200

    initial analysis of score

--- common/bots/canada/expressEntry/analysis.js
+++ common/bots/canada/expressEntry/analysis.js
@@ -1,102 +1,445 @@
 var calculator = require("./calculator");
+var util = require("../../../util");
 
-function analyse(parameters, score) {
+var currentParameters;
+var currentScore;
+
+function analyse(parameters, scores) {
     if (parameters.provincialNomination)
         return "<br /><br />Since you were nominated by a province or territory, you already have enough points to pass the next draw. Welcome to Canada! :)";
-    
-    var analysis = "<br /><br />Here are some things you could do to improve your score.<br /><br />";
 
-    var federalSkilledWorker = validateFederalSkilledWorker(parameters);
-    var skilledTrade = validateSkilledTrade(parameters);
-    var canadianExperience = validateCanadianExperience(parameters);
+    currentParameters = parameters;
+    currentScore = scores;
 
-    if (federalSkilledWorker && skilledTrade && canadianExperience)
-    {
-        analysis = "You are eligible to all three programs: Federal Skilled Worker, Skilled Trade and Canadian Experience.<br />"
+    var analysis = "<br /><br />";
 
+    analysis += "<table class='analysis'>";
+    analysis += "	<caption>Here is a 5-year plan with some ideas to improve your score</caption>";
+    analysis += "	<thead>";
+    analysis += "		<tr>";
+    analysis += "			<th>Current score is: " + util.formatNumber(scores.total, 0) + "</th>";
+    analysis += "			<th>This year</th>";
+    analysis += "			<th>1 year</th>";
+    analysis += "			<th>2 years</th>";
+    analysis += "			<th>3 years</th>";
+    analysis += "			<th>4 years</th>";
+    analysis += "			<th>5 years</th>";
+    analysis += "		</tr>";
+    analysis += "	</thead>";
 
+    analysis += "	<thead>";
+    analysis += "		<tr>";
+    analysis += "			<th colspan='7' class='section'>Keeping your current level on the first official language</th>";
+    analysis += "		</tr>";
+    analysis += "	</thead>";
+
+    analysis += "	<thead>";
+    analysis += "		<tr>";
+    analysis += "			<th colspan='7'>Core/Human capital factors</th>";
+    analysis += "		</tr>";
+    analysis += "	</thead>";
+
+    analysis += age(util.cloneObject(parameters));
+
+    if (parameters.educationLevel < calculator.educationLevel.Secondary)
+        analysis += educationHighSchool(util.cloneObject(parameters));
+
+    if (parameters.educationLevel < calculator.educationLevel.OneYearDegree)
+        analysis += educationOneYearDegree(util.cloneObject(parameters));
+
+    if (parameters.educationLevel < calculator.educationLevel.TwoYearDegree)
+        analysis += educationTwoYearDegree(util.cloneObject(parameters));
+
+    if (parameters.educationLevel < calculator.educationLevel.BachelorsDegree)
+        analysis += educationBachelorsDegree(util.cloneObject(parameters));
+
+    if (parameters.educationLevel < calculator.educationLevel.TwoOrMoreDegress)
+        analysis += educationTwoOrMoreDegress(util.cloneObject(parameters));
+
+    analysis += canadianWorkExperience(util.cloneObject(parameters));
+
+    if (parameters.spouseCommingAlong) {
+        analysis += "	<thead>";
+        analysis += "		<tr>";
+        analysis += "			<th colspan='7'>Spouse factors</th>";
+        analysis += "		</tr>";
+        analysis += "	</thead>";
+
+        if (parameters.spouseEducationLevel < calculator.educationLevel.Secondary)
+            analysis += spouseEducationHighSchool(util.cloneObject(parameters));
+
+        if (parameters.spouseEducationLevel < calculator.educationLevel.OneYearDegree)
+            analysis += spouseEducationOneYearDegree(util.cloneObject(parameters));
+
+        if (parameters.spouseEducationLevel < calculator.educationLevel.TwoYearDegree)
+            analysis += spouseEducationTwoYearDegree(util.cloneObject(parameters));
+
+        if (parameters.spouseEducationLevel < calculator.educationLevel.BachelorsDegree)
+            analysis += spouseEducationBachelorsDegree(util.cloneObject(parameters));
+
+        if (parameters.spouseEducationLevel < calculator.educationLevel.TwoOrMoreDegress)
+            analysis += spouseEducationTwoOrMoreDegress(util.cloneObject(parameters));
+
+        analysis += spouseFirstOfficialLanguage(util.cloneObject(parameters));
+
+        analysis += spouseCanadianWorkExperience(util.cloneObject(parameters));
     }
 
-    /*
-        console.log('federalSkilledWorker: ', federalSkilledWorker);
-        console.log('skilledTrade: ', skilledTrade);
-        console.log('canadianExperience: ', canadianExperience);
-    */
+    analysis += "	<thead>";
+    analysis += "		<tr>";
+    analysis += "			<th colspan='7'>Additional points</th>";
+    analysis += "		</tr>";
+    analysis += "	</thead>";
+
+    analysis += studyInCanadaOneTwoYearDegree(util.cloneObject(parameters));
 
 
+    analysis += "</table><br /><br />";
 
-    //analysis += skilledWorker(parameters, score);
-    //analysis += skilledTrade(parameters, score);
-    //analysis += canadianExperience(parameters, score);
-
-    //var analysisParameters = JSON.parse(JSON.stringify(parameters));
-
-    analysis = "";
-
-    return analysis;
-}
-
-function validateFederalSkilledWorker(parameters) {
-    if (parameters.workExperience < 1)
-        return false;
-
-    if (parameters.educationLevel < 1)
-        return false;
-
-    if (parameters.firstLanguage.speaking < 7 ||
-        parameters.firstLanguage.listening < 7 ||
-        parameters.firstLanguage.reading < 7 ||
-        parameters.firstLanguage.writing < 7)
-        return false;
-
-    return true;
-}
-
-function federalSkilledWorker(parameters) {
-    var analysis = "<br /><br />Skilled Worker:";
+    analysis += "There some other things you could do to get more points, for instance:<br/>";
+    analysis += "If you get a Job Offer in the NOC 00, you'll get 200 more points.<br/>";
+    analysis += "If you get a Job Offer in the NOCs 0, A or B, you'll get 50 more points.<br/>";
+    analysis += "If you get a Certificate of Qualification from a province or territory, you'll get 50 more points.<br/>";
+    analysis += "If you get a Nomination Certificate from a province or territory, you'll get 600 more points.";
 
     return analysis;
 }
 
-function validateSkilledTrade(parameters) {
-    if (parameters.workExperience < 2)
-        return false;
+function age(parameters) {
+    var simulation = "";
 
-    if (!parameters.certificateFromProvince)
-        if (parameters.nocJobOffer === undefined)
-            return false;
-        else
-            if (parameters.nocJobOffer > calculator.nocList.B)
-                return false;
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Age</th>";
 
-    if (parameters.firstLanguage.speaking < 5 ||
-        parameters.firstLanguage.listening < 5 ||
-        parameters.firstLanguage.reading < 4 ||
-        parameters.firstLanguage.writing < 4)
-        return false;
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
 
-    return true;
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
 }
 
-function skilledTrade(parameters) {
-    return "<br /><br />Skilled Trade";
+function educationHighSchool(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: High School</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.educationLevel = calculator.educationLevel.Secondary;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
 }
 
-function validateCanadianExperience(parameters) {
-    if (parameters.workInCanada < 1)
-        return false;
+function educationOneYearDegree(parameters) {
+    var simulation = "";
 
-    if (parameters.firstLanguage.speaking < 4 ||
-        parameters.firstLanguage.listening < 4 ||
-        parameters.firstLanguage.reading < 4 ||
-        parameters.firstLanguage.writing < 4)
-        return false;
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: One-year program</th>";
 
-    return true;
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.educationLevel = calculator.educationLevel.OneYearDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
 }
 
-function canadianExperience(parameters) {
-    return "<br /><br />Canadian Experience Class";
+function educationTwoYearDegree(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: Two-year program</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.educationLevel = calculator.educationLevel.TwoYearDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function educationBachelorsDegree(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: Bachelor\'s degree</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.educationLevel = calculator.educationLevel.BachelorsDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function educationTwoOrMoreDegress(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: Two or more degrees (One is a Bachelor's)</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.educationLevel = calculator.educationLevel.BachelorsDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function canadianWorkExperience(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Canadian work experience</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.workInCanada += 1;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function spouseEducationHighSchool(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: High School</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.spouseEducationLevel = calculator.educationLevel.Secondary;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function spouseEducationOneYearDegree(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: One-year program</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.spouseEducationLevel = calculator.educationLevel.OneYearDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function spouseEducationTwoYearDegree(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: Two-year program</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.spouseEducationLevel = calculator.educationLevel.TwoYearDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function spouseEducationBachelorsDegree(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: Bachelor\'s degree</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.spouseEducationLevel = calculator.educationLevel.BachelorsDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function spouseEducationTwoOrMoreDegress(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Level of education: Two or more degrees (One is a Bachelor's)</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.spouseEducationLevel = calculator.educationLevel.BachelorsDegree;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function spouseFirstOfficialLanguage(parameters) {
+    var simulation = "";
+
+    if (parameters.spouseLanguage.test !== calculator.languageTest.none) {
+        if (parameters.spouseLanguage.speaking < 7 ||
+            parameters.spouseLanguage.listening < 7 ||
+            parameters.spouseLanguage.reading < 7 ||
+            parameters.spouseLanguage.writing < 7) {
+            simulation += "	<tbody>";
+            simulation += "		<tr>";
+            simulation += "			<td>First Official Language to CLB 7</th>";
+
+            for (i = 0; i <= 5; i++) {
+                parameters.age = currentParameters.age + i;
+                parameters.spouseLanguage.speaking = 7;
+                parameters.spouseLanguage.listening = 7;
+                parameters.spouseLanguage.reading = 7;
+                parameters.spouseLanguage.writing = 7;
+
+                calculator.calculate(parameters);
+
+                simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+            }
+
+            simulation += "	</tbody>";
+        }
+
+        if (parameters.spouseLanguage.speaking >= 7 && parameters.spouseLanguage.speaking < 9 ||
+            parameters.spouseLanguage.listening >= 7 && parameters.spouseLanguage.listening < 9 ||
+            parameters.spouseLanguage.reading >= 7 && parameters.spouseLanguage.reading < 9 ||
+            parameters.spouseLanguage.writing >= 7 && parameters.spouseLanguage.writing < 9) {
+            simulation += "	<tbody>";
+            simulation += "		<tr>";
+            simulation += "			<td>First Official Language to CLB 9</th>";
+
+            for (i = 0; i <= 5; i++) {
+                parameters.age = currentParameters.age + i;
+                parameters.spouseLanguage.speaking = 9;
+                parameters.spouseLanguage.listening = 9;
+                parameters.spouseLanguage.reading = 9;
+                parameters.spouseLanguage.writing = 9;
+
+                calculator.calculate(parameters);
+
+                simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+            }
+
+            simulation += "	</tbody>";
+        }
+    }
+
+    return simulation;
+}
+
+function spouseCanadianWorkExperience(parameters) {
+    var simulation = "";
+
+    simulation += "	<tbody>";
+    simulation += "		<tr>";
+    simulation += "			<td>Canadian work experience</th>";
+
+    for (i = 0; i <= 5; i++) {
+        parameters.age = currentParameters.age + i;
+        parameters.spouseWorkInCanada += 1;
+
+        calculator.calculate(parameters);
+
+        simulation += "			<td class='score'>" + util.formatNumber(calculator.scores.total) + scoreDifferente(calculator.scores.total, currentScore.total) + "</td>";
+    }
+
+    simulation += "	</tbody>";
+
+    return simulation;
+}
+
+function scoreDifferente(newScore, currentScore) {
+    var difference = " (";
+
+    difference += (newScore > currentScore ? '+' : '');
+    difference += newScore - currentScore;
+
+    difference += ")";
+
+    return difference;
 }
 
 module.exports = {
