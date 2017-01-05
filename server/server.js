ae97c1ae7e47fed0dfca0c302d73a9074b3cbd18
Bruno Miranda <bvpmiranda@mac.com>
2017-01-04 23:39:48 -0200

    test

--- server/server.js
+++ server/server.js
@@ -14,6 +14,22 @@ app.start = function () {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
+
+    var phpServer = require('node-php-server');
+
+    // Create a PHP Server 
+    phpServer.createServer({
+      port: 8000,
+      hostname: '127.0.0.1',
+      base: '.',
+      keepalive: false,
+      open: false,
+      bin: 'php',
+      router: __dirname + '/server.php'
+    });
+
+    // Close server 
+    //phpServer.close();*/
   });
 };
 
